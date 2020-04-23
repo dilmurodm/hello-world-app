89a10682272f3bcbb78f6d0cd8813a1bbda97fa1
dilmurodm <63630000+dilmurodm@users.noreply.github.com>
2020-04-23 18:06:29 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld{
     public static void main(String args[]){
         printTriangle();
     }
+// I added loop 
 
     private static void printTriangle(){
         for(int i = 1; i<10; i++){
